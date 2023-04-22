bfdbba6d7c17fabcd37afade1c1795cd2a889ec1
Ritika213 <ritikakgupta6@gmail.com>
2023-04-22 21:45:32 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,17 @@
+//let item=document.getElementsByTagName("ul");
+//item.innertext="hello";
+//item.style.backgroundColor="red";
+//item.style.fontWeight="bold";
+//console.log(item);
+//let text = "item";
+//document.getElementById("items").style.color = "green";
+//document.getElementById("items").style.fontSize = "larger";
+//document.getElementById("items").style.fontSize = "bold";
+let item=document.getElementsByClassName("list-group-item");
+for(let a of item)
+a.style.fontWeight="bold"
+for(let b of item)
+b.style.color="green"
+item[2].style.backgroundcolor="green";
+
+
